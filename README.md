d2cb0c542971f968874cd086df6a7551778c871d
unknown <hitoshi.yasuhara@gmail.com>
2020-09-14 18:37:27 +0900

    implement web push api (VAPID)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pwa_sample3
+* Web Push API (vAPID: Voluntary Application Server Identification for Web Push)を実装
--- service-worker.js
+++ service-worker.js
@@ -9,3 +9,15 @@ self.addEventListener('activate', function (e) {
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function (event) { });
+
+self.addEventListener('push', ev => {
+    // payloadの取得
+    const { title, msg, icon } = ev.data.json();
+    self.registration.showNotification(title, {
+        icon: icon,
+        body: msg
+    });
+    self.registration.pushManager.getSubscription().then(subscription => {
+        console.log(subscription);
+    }, err => console.log(err));
+});
